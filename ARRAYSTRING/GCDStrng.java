920dc820a687bb55cbbe99f8f9a498e1b732f0e0
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-07-16 22:17:05 +0530

    c

--- ARRAYSTRING/GCDStrng.java
+++ ARRAYSTRING/GCDStrng.java
@@ -1,5 +1,37 @@
 package ARRAYSTRING;
 
 public class GCDStrng {
-    
+    public static void main(String[] args) {
+        
+    }
+    public String gcdOfStrings(String str1, String str2){
+        if(!(str1+str2).equals(str2+str1)){   //which means str1 != str2+str2+str2+str2.................
+            return " ";
+        }
+        int a = str1.length();
+        int b = str2.length();
+        int gcdLength = gcd(str1.length(), str2.length());
+
+        
+        //part of string whch can divded both strings.
+        return str1.substring(0, gcdLength); 
+
+    }
+
+
+    public static  int gcd(int a   , int b ){
+        while(b>0){
+            int temp = b ;
+            b = a%b;
+            a= temp;
+        }
+        return a;
+    }  // EUCLIDEAN FORMULA: gcd(a,b) == gcd(b , a % b)
+    //another way : 
+    public static int gcd1(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
 }
